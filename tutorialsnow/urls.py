34bccdf916d9dc26307af165c83201d0884c8941
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 02:18:18 +0300

    Changed the path using pk

--- tutorialsnow/urls.py
+++ tutorialsnow/urls.py
@@ -3,6 +3,6 @@ from tutorialsnow import views
  
 urlpatterns = [ 
     path('api/tutorials', views.tutorial_list),
-    path('api/tutorials/(?P<pk>[0-9]+)', views.tutorial_detail),
+    path('api/tutorials/<int:pk>', views.tutorial_detail),
     path('api/tutorials/published', views.tutorial_list_published)
 ]
