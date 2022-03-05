dd4d6100f1ce57d4c149725dae38b6ddf55c40ab
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 01:11:58 +0300

    Gets, posts and deletes all tutorials

--- tutorialsnow/views.py
+++ tutorialsnow/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET', 'POST', 'DELETE'])
+def tutorial_list(request):
+    # GET list of tutorials, POST a new tutorial, DELETE all tutorials
+    return
