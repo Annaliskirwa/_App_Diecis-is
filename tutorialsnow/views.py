0d95be3bdc022d8a268f1f7921a091c9ed4eb0a8
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 01:19:06 +0300

    Get, put and delete a tutorial by id

--- tutorialsnow/views.py
+++ tutorialsnow/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+from tutorialsnow.models import Tutorial
+from tutorialsnow.serializers import TutorialSerializer
 
 # Create your views here.
 @api_view(['GET', 'POST', 'DELETE'])
 def tutorial_list(request):
     # GET list of tutorials, POST a new tutorial, DELETE all tutorials
     return
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def tutorial_detail(request, pk):
+    # find tutorial by pk (id)
+    try: 
+        tutorial = Tutorial.objects.get(pk=pk) 
+    except Tutorial.DoesNotExist: 
+        return JsonResponse({'message': 'Ooops! The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+ 
+    # GET / PUT / DELETE tutorial
