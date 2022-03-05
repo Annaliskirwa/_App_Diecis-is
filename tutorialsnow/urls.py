64ae3045a6d498274e6cbd3e82494be2385ecb31
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 02:01:45 +0300

    Brushed on the urls

--- tutorialsnow/urls.py
+++ tutorialsnow/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import path
 from tutorialsnow import views 
  
 urlpatterns = [ 
-    path('api/tutorials$', views.tutorial_list),
-    path('api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
-    path('api/tutorials/published$', views.tutorial_list_published)
+    path('api/tutorials', views.tutorial_list),
+    path('api/tutorials/(?P<pk>[0-9]+)', views.tutorial_detail),
+    path('api/tutorials/published', views.tutorial_list_published)
 ]
