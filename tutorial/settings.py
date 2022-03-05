7523f7e91fc9e1ef0f5f58ebf05422324284d7c3
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 00:00:37 +0300

    Changed the database declaration

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -74,10 +74,13 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutorial',
+        'USER': 'annalis',
+    'PASSWORD':'Ann123',
     }
 }
 
