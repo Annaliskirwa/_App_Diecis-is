acc756130b650011f4c77f88a2e0935308c8cb8b
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-06 01:41:44 +0300

    Method for creating and saving a new tutorial

--- tutorialsnow/views.py
+++ tutorialsnow/views.py
@@ -10,7 +10,7 @@ from tutorialsnow.serializers import TutorialSerializer
 @api_view(['GET', 'POST', 'DELETE'])
 def tutorial_list(request):
     # GET list of tutorials, POST a new tutorial, DELETE all tutorials
-     if request.method == 'GET':
+    if request.method == 'GET':
         tutorials = Tutorial.objects.all()
         
         title = request.GET.get('title', None)
@@ -20,6 +20,14 @@ def tutorial_list(request):
         tutorials_serializer = TutorialSerializer(tutorials, many=True)
         return JsonResponse(tutorials_serializer.data, safe=False)
         # 'safe=False' for objects serialization
+
+    elif request.method == 'POST':
+        tutorial_data = JSONParser().parse(request)
+        tutorial_serializer = TutorialSerializer(data=tutorial_data)
+        if tutorial_serializer.is_valid():
+            tutorial_serializer.save()
+            return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def tutorial_detail(request, pk):
