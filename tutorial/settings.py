ed05965996f11f308509fba3303b4a8b9c78bca7
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-07 04:42:07 +0300

    Update database configs in prod

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+import dj_database_url 
 
 from pathlib import Path
 
@@ -148,3 +149,6 @@ STATICFILES_DIRS = (
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
